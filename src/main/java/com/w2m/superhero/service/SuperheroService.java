f805439d7a3e88730a7cc2a563fb786b4d00922d
Rommel <rommel.torrez@mercadolibre.com>
2025-07-30 17:28:10 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,1 +1,9 @@
-quarkus.log.console.format=%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c{2.}] (%t) %s%e%n
+category:
+      "org.jbpm.workflow.instance.impl.NodeInstanceImpl":
+        level: TRACE
+      "org.kie.kogito.process.impl":  # Para ver errores de procesos
+        level: TRACE
+      "org.kie.kogito.services":  # Para ver servicios BPM
+        level: DEBUG
+      "com.santander":  # Ajusta a tu paquete base
+        level: DEBUG
