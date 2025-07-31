45e27b2a6b44021d0afc94d35bbc1b830b79bdde
Rommel <rommel.torrez@mercadolibre.com>
2025-07-31 13:03:13 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,21 +1,3 @@
-format: "%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c{2.}] (%t) %s%e%n"
-
-
-
-log:
-    level: INFO
-    category:
-      "org.jbpm.workflow.instance.impl":
-        level: DEBUG
-      "org.jbpm.workflow.instance.node":
-        level: DEBUG
-      "org.jbpm.workflow.core":
-        level: DEBUG
-      "org.jbpm.process.instance":
-        level: DEBUG
-      "org.kie.kogito.process.impl":
-        level: DEBUG
-      "org.kie.kogito.process":
-        level: DEBUG
-      "org.kie.kogito.quarkus":
-        level: DEBUG
+return specificDataTask != null && 
+       specificDataTask.getPrevious() != null && 
+       specificDataTask.getPrevious().toString().equals("true");
