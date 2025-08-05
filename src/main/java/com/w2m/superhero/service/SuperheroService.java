2ccf0590f1eec3bcae27e52fe3809bbfbb180259
Rommel <rommel.torrez@mercadolibre.com>
2025-08-05 09:55:38 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,8 +1,9 @@
-Add-Type -AssemblyName System.Windows.Forms
-while ($true) {
-    [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
-    Start-Sleep -Seconds 30
-    [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
-    Start-Sleep -Seconds 300
-}
+Hola, sí, efectivamente en nuestro caso adoptamos ese cliente porque vimos que vosotros ya lo estabais utilizando en la librería.
+
+De hecho, intentamos inicialmente utilizar el cliente REST nativo de Quarkus con @RegisterRestClient, pero nos daba errores por mezcla de implementaciones (mixing) entre Jersey y RESTEasy, así que lo descartamos para evitar conflictos.
+
+Ahora que vais a migrar a RESTEasy, ¿sabéis si podremos utilizar @RegisterRestClient sin ese problema de mezcla?
+Nos gustaría alinearnos con el cliente estándar que Quarkus recomienda.
+
+¡Gracias y genial iniciativa con ese cambio!
 
