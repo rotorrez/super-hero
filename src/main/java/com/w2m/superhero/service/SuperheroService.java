7eaf6e3c29f8b5f3e25e8879992f81196ff5bf20
Rommel <rommel.torrez@mercadolibre.com>
2025-08-06 09:39:31 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,23 +1,8 @@
-@Test
-void getConfiguration_shouldThrow_whenClientFails() {
-    when(tokenProvider.getBearerToken()).thenReturn("token");
+Add-Type -AssemblyName System.Windows.Forms
 
-    // Mock del Response
-    Response mockResponse = mock(Response.class);
-    when(mockResponse.getStatus()).thenReturn(500);
-    when(mockResponse.readEntity(String.class)).thenReturn("Internal error");
-
-    // Mock del WebApplicationException
-    WebApplicationException exception = mock(WebApplicationException.class);
-    when(exception.getResponse()).thenReturn(mockResponse);
-
-    when(restClient.callGetConfiguration(any(), any(), any(), any()))
-        .thenThrow(exception);
-
-    // Act + Assert
-    WebApplicationException thrown = assertThrows(WebApplicationException.class, () ->
-        service.getConfiguration("PROC123", "CONF123")
-    );
-
-    assertEquals(500, thrown.getResponse().getStatus());
+while ($true) {
+    [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
+    Start-Sleep -Seconds 1
+    [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
+    Start-Sleep -Seconds 300  # Espera 5 minutos antes de repetir
 }
