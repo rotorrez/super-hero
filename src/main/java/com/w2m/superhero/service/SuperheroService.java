f2fd426ff487e74472ef4192edc020ec5c03ca8d
Rommel <rommel.torrez@mercadolibre.com>
2025-08-07 11:17:59 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,30 +1,3 @@
-<dependency>
-  <groupId>com.santander.sgt.apm1953</groupId>
-  <artifactId>sgt-apm1953-ppbm</artifactId>
-  <version>1.0.1</version>
-  <exclusions>
-    <exclusion>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-resteasy</artifactId>
-    </exclusion>
-    <exclusion>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-resteasy-jackson</artifactId>
-    </exclusion>
-    <!-- Agrega esto también -->
-    <exclusion>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-resteasy-server-common</artifactId>
-    </exclusion>
-  </exclusions>
-</dependency>
-
-
-  <dependency>
-  <groupId>io.quarkus</groupId>
-  <artifactId>quarkus-rest</artifactId>
-</dependency>
-<dependency>
-  <groupId>io.quarkus</groupId>
-  <artifactId>quarkus-rest-jackson</artifactId>
-</dependency>
+mvn clean
+rm -rf target
+rm -rf ~/.m2/repository/io/quarkus
