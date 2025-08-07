96d99a6345a2b7ae476c4ae679291e648bb8128d
Rommel <rommel.torrez@mercadolibre.com>
2025-08-07 13:14:27 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,1 +1,26 @@
-find . -name "*.jar" -exec sh -c 'jar tf {} | grep -q QuarkusRestPathTemplate && echo {}' \;
+<dependencyManagement>
+  <dependencies>
+    <!-- BOM de Quarkus -->
+    <dependency>
+      <groupId>io.quarkus</groupId>
+      <artifactId>quarkus-bom</artifactId>
+      <version>${quarkus.platform.version}</version>
+      <type>pom</type>
+      <scope>import</scope>
+    </dependency>
+
+    <!-- Excluir resteasy desde cualquier sitio -->
+    <dependency>
+      <groupId>io.quarkus</groupId>
+      <artifactId>quarkus-resteasy</artifactId>
+      <version>${quarkus.platform.version}</version>
+      <scope>provided</scope>
+      <exclusions>
+        <exclusion>
+          <groupId>*</groupId>
+          <artifactId>*</artifactId>
+        </exclusion>
+      </exclusions>
+    </dependency>
+  </dependencies>
+</dependencyManagement>
