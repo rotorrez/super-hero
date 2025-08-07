01df7b754424433154a60ac96a23278beed25bb3
Rommel <rommel.torrez@mercadolibre.com>
2025-08-07 12:11:17 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,6 +1,15 @@
-<dependency>
-    <groupId>io.quarkus</groupId>
-    <artifactId>quarkus-resteasy</artifactId>
-    <version>${quarkus.platform.version}</version>
-    <scope>provided</scope> <!-- O test -->
-</dependency>
+cd ~/.m2/repository
+find . -name "*.jar" -exec jar tf {} \; | grep QuarkusRestPathTemplate
+
+
+    $search = "QuarkusRestPathTemplate"
+$repo = "$env:USERPROFILE\.m2\repository"
+
+Get-ChildItem -Path $repo -Recurse -Include *.jar | ForEach-Object {
+    try {
+        $jarContent = & "jar" "tf" $_.FullName 2>$null
+        if ($jarContent -match $search) {
+            Write-Host "FOUND in:" $_.FullName
+        }
+    } catch {}
+}
