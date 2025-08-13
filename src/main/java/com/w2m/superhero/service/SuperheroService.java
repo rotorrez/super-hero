87dcfb0a2f1f9af84eca4e9bf7ea0edfc49684ea
Rommel <rommel.torrez@mercadolibre.com>
2025-08-13 14:33:54 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,15 +1,2 @@
-import org.jboss.resteasy.annotations.jaxrs.PATCH;
-
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
-public interface AppianCaseAndGoRestClient {
-
-    @PATCH
-    @Path("/{dynamicPart}/start-fast-process")
-    StartFastProcessResponseDTO startFastProcess(
-        @HeaderParam("Authorization") String bearer,
-        @HeaderParam("X-ClientId") String clientId,
-        StartFastProcessRequestDTO body,
-        @PathParam("dynamicPart") String dynamicPath
-    );
-}
+quarkus.log.category."org.jboss.resteasy.client.jaxrs".level=DEBUG
+quarkus.log.category."org.jboss.resteasy.microprofile.client".level=DEBUG
