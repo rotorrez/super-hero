b682732def93639b3b091be78dd28d13d1c719f1
Rommel <rommel.torrez@mercadolibre.com>
2025-08-13 13:14:37 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,2 +1,13 @@
-@RegisterRestClient(configKey = "my-service", baseUri = "https://default.example.com")
-public interface MyServiceRestClient { ... }
+# Dentro del JAR de la librería
+quarkus.rest-client.appian-case-and-go-client.url=${bamoecoexis.appian-case-and-go.base-url:https://default.invalid}
+
+
+
+@RegisterRestClient(
+    configKey = "appian-case-and-go-client",
+    baseUri   = "https://default.invalid" // Fallback si no hay config
+)
+
+
+ @ConfigProperty(name = "bamoecoexis.appian-case-and-go.client-id")
+    Optional<String> clientId;
