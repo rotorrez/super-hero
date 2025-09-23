99b32441fadb0c6222e6fd0a6e3a8d2bbb856f14
Rommel <rommel.torrez@mercadolibre.com>
2025-09-23 11:30:28 +0200

    Update SuperheroService.java

--- src/main/java/com/w2m/superhero/service/SuperheroService.java
+++ src/main/java/com/w2m/superhero/service/SuperheroService.java
@@ -1,5 +1,5 @@
-git tag -a v1.0-archived -m "Stable state before project was put on hold"
+Project Status
 
-
-This project is currently on hold.  
-Last stable state: [v1.0-archived](../../releases/tag/v1.0-archived)
+This project is currently on hold.
+The last stable codebase has been tagged as v1.0-on-hold
+.
